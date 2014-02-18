4705e8a7d101d9b7fa9a71acc77acc295aa62552
nekosune <kat.swales@nekokittygames.com>
2014-02-18 12:15:17 +0000

    Basic Skelington

---
+++ VirtualChestMod/src/main/java/com/feedthebeast/VirtualChest/VirtualChestMod.java
@@ -0,0 +1,35 @@
+package com.feedthebeast.VirtualChest;
+
+import java.util.logging.Logger;
+
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.event.ForgeSubscribe;
+
+import com.feedthebeast.VirtualChest.lib.LibMisc;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid=LibMisc.MODID,name=LibMisc.MODNAME,version=LibMisc.VERSION)
+@NetworkMod(clientSideRequired=true)
+public class VirtualChestMod {
+	@Instance(LibMisc.MODID)
+	public static VirtualChestMod instance;
+
+
+	public static Logger logger;
+	
+	@ForgeSubscribe
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		logger = event.getModLog();
+		logger.setParent(FMLLog.getLogger());
+
+		Configuration config = new Configuration(
+				event.getSuggestedConfigurationFile());
+		Configs.load(config);
+	}
+}
---
+++ VirtualChestMod/src/main/java/com/feedthebeast/VirtualChest/lib/LibMisc.java
@@ -0,0 +1,8 @@
+package com.feedthebeast.VirtualChest.lib;
+
+public class LibMisc {
+
+	public final static String VERSION="${version}";
+	public final static String MODID="ftbvirtualchest";
+	public final static String MODNAME="Virtual Chest";
+}
